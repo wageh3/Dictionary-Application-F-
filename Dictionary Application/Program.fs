e65a6f03049b148c947a168f18a20c714cf9379a
Wageh_Mostafa <120650371+wageh3@users.noreply.github.com>
2025-12-03 00:52:54 +0200

    CRUD Operations

--- Dictionary Application/Program.fs
+++ Dictionary Application/Program.fs
@@ -1,2 +1,35 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Dictionary_Application.Models
+open Dictionary_Application.Services
+
+[<EntryPoint>]
+let main _ =
+    printfn "Testing CRUD functions...\n"
+
+    
+    let dict0 = Map.empty<string, Word>
+
+    
+    let dict1 =
+        CRUD.addWord "Apple" "A kind of fruit" dict0
+
+    printfn "After Add Word: %A\n" dict1
+
+    
+    match CRUD.getWord "Apple" dict1 with
+    | Some w -> printfn "Found: %s = %s\n" w.Term w.Definition
+    | None -> printfn "Not found!\n"
+
+   
+    let dict2 =
+        CRUD.updateWord "Apple" "A sweet red or green fruit" dict1
+
+    printfn "After Update: %A\n" dict2
+
+  
+    let dict3 =
+        CRUD.deleteWord "Apple" dict2
+
+    printfn "After Delete: %A\n" dict3
+
+    0
+    
--- Dictionary Application/Services/CRUD.fs
+++ Dictionary Application/Services/CRUD.fs
@@ -1,2 +1,35 @@
-﻿module CRUD
+﻿namespace Dictionary_Application.Services
 
+open Dictionary_Application.Models
+
+module CRUD =
+
+    /// Add a new word.
+    let addWord (term: string) (definition: string) (dict: Map<string, Word>) =
+        let key = term.ToLower()
+        let newWord = { Term = term; Definition = definition }
+        dict |> Map.add key newWord
+
+
+    /// Update
+    let updateWord (term: string) (newDefinition: string) (dict: Map<string, Word>) =
+        let key = term.ToLower()
+
+        match dict.TryFind key with
+        | Some existing ->
+            let updated = { existing with Definition = newDefinition }
+            dict |> Map.add key updated
+        | None ->
+            dict   
+
+
+    /// Delete 
+    let deleteWord (term: string) (dict: Map<string, Word>) =
+        let key = term.ToLower()
+        dict |> Map.remove key
+
+
+    /// Get a word
+    let getWord (term: string) (dict: Map<string, Word>) =
+        let key = term.ToLower()
+        dict |> Map.tryFind key
