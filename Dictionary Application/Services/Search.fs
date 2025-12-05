15c86eebe609d4c0a084af4037e46639ca339d7b
Wageh_Mostafa <120650371+wageh3@users.noreply.github.com>
2025-12-05 19:21:26 +0200

    Add Search Funcs ,some enhancement in CRUD and add load in program file

--- Dictionary Application/Services/Search.fs
+++ Dictionary Application/Services/Search.fs
@@ -1,2 +1,20 @@
 ﻿module Search
 
+
+open Dictionary_Application.Models 
+open System
+
+let caseInsensitiveSearch (term: string) (dict: Map<string, Word>) =
+    let searchKey = term.Trim().ToLower()
+    dict |> Map.tryFind searchKey
+
+let partialSearch (pattern: string) (dict: Map<string, Word>) =
+    if String.IsNullOrWhiteSpace(pattern) then
+        []
+    else
+        let searchPattern = pattern.Trim().ToLower()
+        
+        dict
+        |> Map.toList
+        |> List.filter (fun (k, _) -> k.Contains(searchPattern))
+        |> List.map snd
