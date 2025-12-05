e60c6e346e32bd39ee7997689a1d3d7126affc22
Wageh_Mostafa <120650371+wageh3@users.noreply.github.com>
2025-12-05 19:28:13 +0200

    add first parts of menu to avoid errors

--- Dictionary Application/UI/Menu.fs
+++ Dictionary Application/UI/Menu.fs
@@ -1,2 +1,7 @@
-﻿module Menu
+﻿namespace Dictionary_Application.UI
+open Dictionary_Application 
+open Dictionary_Application.Models
 
+module Menu =
+    let rec loop (dict: Map<string, Word>) =
+        printfn "\n--- Dictionary Menu ---"
